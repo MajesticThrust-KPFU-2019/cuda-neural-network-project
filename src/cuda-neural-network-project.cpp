e27754f7bfb87a9caacb57786e456eb67e14025b
MajesticThrust <majesticthrust@gmail.com>
2020-08-27 22:38:52 +0500

    Initial commit

---
+++ src/cuda-neural-network-project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cuda-neural-network-project.cpp
+// Author      : Timur Mukhametulin
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
